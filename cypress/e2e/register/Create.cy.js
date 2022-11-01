58800c1ff67df2fad67b53cc90298cccfc138b39
feroline <anacarolina.rrocha@gmail.com>
2022-10-31 22:39:20 -0300

    iniciando projeto

---
+++ cypress/e2e/register/Create.cy.js
@@ -0,0 +1,39 @@
+/// <reference types="cypress"/>
+
+import registerLocators from "../../support/locators/register"; 
+
+context('Create', () =>  {
+    before(() => {
+        cy.fixture('./persona').as('persona');
+    });
+    
+    beforeEach(() => {
+        cy.on('uncaught:exception', (err, runnable) => {
+            return false;
+        });
+
+       // cy.visit('https://demo.automationtesting.in/Register.html');
+    });
+
+    it('Verify URL', () => {
+        console.log(cy.url().should('to.be.equal', 'https://demo.automationtesting.in/Register.html'));
+    });
+
+    it.only('Valid data', () => {
+        cy.visit('https://demo.automationtesting.in/Register.html');
+
+        cy.get('@persona').then(data => {
+        
+            cy.get(registerLocators.INPUT_FIST_NAME).type(data.first_name);
+            cy.get(registerLocators.INPUT_FIST_NAME).should('have.value', data.first_name);
+
+            cy.get(registerLocators.INPUT_LAST_NAME).type(data.last_name);
+            cy.get(registerLocators.INPUT_LAST_NAME).should('have.value',data.last_name);
+
+            cy.get(registerLocators.TEXTAREA_ADRESS).type(data.adress);
+            cy.get(registerLocators.TEXTAREA_ADRESS).contains(data.adress);
+        });
+
+    });
+
+});
---
+++ cypress/support/locators/register.js
@@ -0,0 +1,8 @@
+const registerLocators = {
+    INPUT_FIST_NAME: 'input[type="text"][ng-model="FirstName"]',
+    INPUT_LAST_NAME: 'input[type="text"][ng-model="LastName"]',
+    TEXTAREA_ADRESS: 'textarea[ng-model="Adress"]'
+
+}; 
+ 
+export default registerLocators;
