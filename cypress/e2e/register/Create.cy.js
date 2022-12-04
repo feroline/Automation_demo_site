254353b42f9fb41a76aad2c62ae52c9eb99cf100
feroline <anacarolina.rrocha@gmail.com>
2022-12-04 18:28:07 -0300

    adicionando até o gender

--- cypress/e2e/register/Create.cy.js
+++ cypress/e2e/register/Create.cy.js
@@ -3,7 +3,7 @@
 import registerLocators from "../../support/locators/register"; 
 
 context('Create', () =>  {
-    
+
     before(() => {
         cy.fixture('./persona').as('persona');
     });
@@ -12,15 +12,24 @@ context('Create', () =>  {
 
         cy.get('@persona').then(data => {
         
-            cy.get(registerLocators.INPUT_FIST_NAME).type(data.first_name);
+            cy.get(registerLocators.INPUT_FIST_NAME).type(data.first_name).as('First Name');
             cy.get(registerLocators.INPUT_FIST_NAME).should('have.value', data.first_name);
 
-            cy.get(registerLocators.INPUT_LAST_NAME).type(data.last_name);
+            cy.get(registerLocators.INPUT_LAST_NAME).type(data.last_name).as('Last Name');
             cy.get(registerLocators.INPUT_LAST_NAME).should('have.value',data.last_name);
 
-            cy.get(registerLocators.TEXTAREA_ADRESS).type(data.adress);
-            // cy.get(registerLocators.TEXTAREA_ADRESS).type(data.adress);
-            cy.get(registerLocators.TEXTAREA_ADRESS).should('have.value',data.adress);
+            cy.get(registerLocators.TEXTAREA_ADRESS).type(data.adress).as('Address');
+            // TODO: Ver depois porque esse trem tá vindo quebrado; 
+            // cy.get(registerLocators.TEXTAREA_ADRESS).should('have.value',data.adress); 
+            
+            cy.get(registerLocators.INPUT_EMAIL).type(data.email).as('Email');
+            cy.get(registerLocators.INPUT_EMAIL).should('have.value',data.email);
+            
+            cy.get(registerLocators.INPUT_PHONE).type(data.phone).as('Phone');
+            cy.get(registerLocators.INPUT_PHONE).should('have.value',data.phone);
+
+            cy.get(registerLocators.FUNC_INPUT_GENDER(data.gender)).type(data.gender).as('Gender');
+            cy.get(registerLocators.FUNC_INPUT_GENDER(data.gender)).check();
         });
 
     });
--- cypress/support/locators/register.js
+++ cypress/support/locators/register.js
@@ -1,7 +1,26 @@
+import { empty } from "check-more-types";
+import { xor } from "lodash";
+
 const registerLocators = {
     INPUT_FIST_NAME: 'input[type="text"][ng-model="FirstName"]',
     INPUT_LAST_NAME: 'input[type="text"][ng-model="LastName"]',
-    TEXTAREA_ADRESS: 'textarea[ng-model="Adress"]'
+    TEXTAREA_ADRESS: 'textarea[ng-model="Adress"]',
+    INPUT_EMAIL: 'input[type="email"][ng-model="EmailAdress"]',
+    INPUT_PHONE: 'input[type="tel"][ng-model="Phone"]',
+    _getGender(gender){
+        if (gender != null && gender && empty && gender != " ")
+            return gender == 'female' ? 'FeMale' : 'Male';
+        return gender;
+    },
+    FUNC_INPUT_GENDER(gender){
+        
+        return `input[type="radio"][ng-model="radiovalue"][value="${this._getGender(gender)}"]`
+        // document.querySelector(`input[type="radio"][ng-model="radiovalue"][value=${selectedGender}]`);
+        // <input type="radio" name="radiooptions" ng-model="radiovalue" value="FeMale"></input>
+    },
+        
+        INPUT_GENDER_FEMALE:'female', 
+        INPUT_GENDER_MALE:'male'
 
 }; 
  
